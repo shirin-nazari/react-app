a09d1db7e193b206efc48596c33e8dd8bc954a1d
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-08-09 02:15:57 +0430

    add style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { useEffect } from 'react';
+import './App.css';
 // 109898eb
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=109898eb';
+const API_URL = 'http://www.omdbapi.com/?apikey=109898eb';
 const App = () => {
-  useEffect(() => {}, []);
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+  };
+  // useEffect(() => {
+  //   searchMovies('Spiderman');
+  // }, []);
   return <h1>app</h1>;
 };
 export default App;
