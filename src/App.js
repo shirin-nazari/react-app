ed61a2fc82d0fffb0e28cbe3002f03f22a805bfd
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2022-08-09 03:21:22 +0430

    finish

--- src/App.js
+++ src/App.js
@@ -1,16 +1,59 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import SearchIcon from './noun-search.svg';
+import MovieCard from './MovieCard';
 // 109898eb
 const API_URL = 'http://www.omdbapi.com/?apikey=109898eb';
 const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data.Search);
+    setMovies(data.Search);
   };
-  // useEffect(() => {
-  //   searchMovies('Spiderman');
-  // }, []);
-  return <h1>app</h1>;
+  const movie1 = {
+    Title: 'The Amazing Spiderman T4 Premiere Special',
+    Year: '2012',
+    imdbID: 'tt2233044',
+    Type: 'movie',
+    Poster: 'N/A',
+  };
+  useEffect(() => {
+    searchMovies('Spiderman');
+  }, []);
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search for movie"
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => {
+            searchMovies(searchTerm);
+          }}
+        />
+      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No Movies found</h2>
+        </div>
+      )}
+    </div>
+  );
 };
 export default App;
